d9f060721fcdfb0b1af7f88372d0bb960a889ae2
33bca <fabian.leutenegger@bluewin.ch>
2016-10-05 21:24:34 +0200

    Disable IMS and CAF RIL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,7 +170,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 # RIL
 PROTOBUF_SUPPORTED := true
-TARGET_RIL_VARIANT := caf
+#TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
