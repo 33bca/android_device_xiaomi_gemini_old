aca343abe583e01f6f45e97f4212af5b52105493
33bca <fabian.leutenegger@bluewin.ch>
2016-12-11 21:20:28 +0100

    gemini: AOSPA-Bringup

--- device.mk
+++ device.mk
@@ -38,7 +38,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # HWUI overrides
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+$(call inherit-product, device/xiaomi/gemini/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
